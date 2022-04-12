0aa7c7a13ad0b2b977916a75cc5aecd1cd1d142b
ralexrivero <ralexrivero@gmail.com>
2022-04-12 06:27:15 +0000

    list comprohensions for transpose

---
+++ 0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" transpose matrix """
+
+def matrix_transpose(matrix):
+    """ transpose matrix """
+    transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    return transposed
