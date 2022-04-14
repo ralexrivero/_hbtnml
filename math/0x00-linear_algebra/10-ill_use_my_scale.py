25864ae20ae588a2a8850b38b992bc5a233b9003
ralexrivero <ralexrivero@gmail.com>
2022-04-14 22:18:01 +0000

    numpy shape

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" calculate the shape """
+import numpy as np
+
+
+def np_shape(matrix):
+    """ matrix shape """
+    npArray = np.array(matrix)
+    return npArray.shape
