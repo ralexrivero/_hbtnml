c5ba566b2431c311a525b682b21f7a0f24543f1d
ralexrivero <ralexrivero@gmail.com>
2022-04-19 04:02:27 +0000

    tight layout

--- math/0x01-plotting/5-all_in_one.py
+++ math/0x01-plotting/5-all_in_one.py
@@ -32,41 +32,45 @@ plt2 = plt.subplot2grid((3, 2), (1, 0), rowspan=1)
 plt3 = plt.subplot2grid((3, 2), (1, 1), rowspan=1)
 plt4 = plt.subplot2grid((3, 2), (2, 0), colspan=2)
 
+""" global settings """
+plt.rcParams.update({'axes.titlesize': 'x-small'})
+plt.rcParams.update({'axes.labelsize': 'x-small'})
 
 """ 0 """
-plt.title('All in One', size='x-small')
+plt.title('All in One')
 plt0.plot(np.arange(0, 11), y0, 'r-')
 
 """ 1 """
 plt1.scatter(x1, y1, color='m')
-plt1.set_title('Scatter Plot', size='x-small')
-plt1.set_xlabel('Height (in)', size='x-small')
-plt1.set_ylabel('Weight (lbs)', size='x-small')
-plt1.set_title("Men's Height vs Weight", size='x-small')
+plt1.set_title('Scatter Plot')
+plt1.set_xlabel('Height (in)')
+plt1.set_ylabel('Weight (lbs)')
+plt1.set_title("Men's Height vs Weight")
 
 """ 2 """
 plt2.plot(x2, y2)
-plt2.set_xlabel('Time (years)', size='x-small')
-plt2.set_ylabel('Fraction Remaining', size='x-small')
-plt2.set_title('Exponential Decay of C-14', size='x-small')
+plt2.set_xlabel('Time (years)')
+plt2.set_ylabel('Fraction Remaining')
+plt2.set_title('Exponential Decay of C-14')
 plt2.set_yscale('log')
 plt2.set_xlim(0, 28650)
 
 """ 3 """
 plt3.plot(x3, y31, 'r--')
 plt3.plot(x3, y32, 'g-')
-plt3.set_xlabel('Time (years)', size='x-small')
-plt3.set_ylabel('Fraction Remaining', size='x-small')
-plt3.set_title('Exponential Decay of Radioactive Elements',
-                     size='x-small')
+plt3.set_xlabel('Time (years)')
+plt3.set_ylabel('Fraction Remaining')
+plt3.set_title('Exponential Decay of Radioactive Elements')
 plt3.set_xlim(0, 20000)
 plt3.set_ylim(0, 1)
 plt3.legend(['C-14', 'Ra-226'])
 
 """ 4 """
 plt4.hist(student_grades, bins=10, color='k')
-plt4.set_xlabel('Grades', size='x-small')
-plt4.set_ylabel('Number of Students', size='x-small')
-plt4.set_title('Project A', size='x-small')
+plt4.set_xlabel('Grades')
+plt4.set_ylabel('Number of Students')
+plt4.set_title('Project A')
 
+""" pack and display plots """
+plt.tight_layout()
 plt.show()
