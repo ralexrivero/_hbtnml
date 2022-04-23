40fc52c84c4750742fa849cd0c616c3cf4127725
ralexrivero <ralexrivero@gmail.com>
2022-04-23 17:52:44 +0000

    matrix multiplication

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -6,10 +6,18 @@ def mat_mul(mat1, mat2):
     """ matrix multiplication """
     if len(mat1[0]) != len(mat2):
         return None
-    res = []
 
-    for row1 in range(len(mat1)):
-        for col2 in range(len(mat2[0])):
-            for row2 in range(len(mat2)):
-                print(mat1[][], mat2[][])
+    res = []
+    """ for i in range(len(mat1)):
+        res.append([])
+        for j in range(len(mat2[0])):
+            res[i].append(0) """
+
+    for i in range(len(mat1)):
+        res.append([])
+        for j in range(len(mat2[0])):
+            res[i].append(0)
+            for k in range(len(mat2)):
+                res[i][j] += mat1[i][k] * mat2[k][j]
+
     return res
