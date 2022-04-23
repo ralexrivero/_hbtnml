70c90f68f744a9785c7283efbf7072c35e7d8dd5
ralexrivero <ralexrivero@gmail.com>
2022-04-23 03:17:47 +0000

    matrix concatenation

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
 """ concatenates two matrices """
-matCon = []
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """ concatenates two matrices """
     if axis == 0:
-        matCon.clear()
+        matCon = []
+        for row in mat1:
+            matCon.append(row.copy())
+        for row in mat2:
+            matCon.append(row.copy())
+    else:
+        matCon = []
         for i in range(len(mat1)):
-            matCon.append(mat1[i])
-        for j in range(len(mat2)):
-            matCon.append(mat2[j])
-        return matCon
-    if axis == 1:
-        matCon.clear()
-        for i in range(len(mat1)):
-            matCon.append(mat1[i])
-        for j in range(len(mat2)):
-            for k in range(len(mat2[j])):
-                matCon[j].append(mat2[j][k])
-        return matCon
+            matCon.append([])
+            matCon[i] = mat1[i] + mat2[i]
+    return matCon
