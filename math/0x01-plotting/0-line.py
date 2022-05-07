75f49c364e7727d7befd39372bad52fbf15b2f31
ralexrivero <ralexrivero@gmail.com>
2022-05-07 06:09:30 +0000

    plot color red 'r'

--- math/0x01-plotting/0-line.py
+++ math/0x01-plotting/0-line.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Plot a line """
+from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
 
-plt.plot(np.arange(0, 11), y)
+plt.plot(np.arange(0, 11), y, 'r')
 plt.show()
