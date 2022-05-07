b9bdbd4f4948f802d5d2f424c82cea5a9678a245
ralexrivero <ralexrivero@gmail.com>
2022-05-07 07:22:48 +0000

    configure bars

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-plt.hist(student_grades, bins=10, color='k')
+
 plt.xlabel('Grades')
 plt.ylabel('Number of Students')
 plt.title('Project A')
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.locator_params('x', nbins = 10)
+plt.hist(student_grades, bins=10, color='#228BC2', edgecolor='k', range=(0, 100))
 plt.show()
