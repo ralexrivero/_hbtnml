ed72da31ba34ea84359b6a6626f413e63285edb7
ralexrivero <ralexrivero@gmail.com>
2022-05-09 18:29:46 +0000

    scatter gradient

--- math/0x01-plotting/100-gradient.py
+++ math/0x01-plotting/100-gradient.py
@@ -8,7 +8,7 @@ x = np.random.randn(2000) * 10
 y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-plt.scatter(x,y,z)
+plt.scatter(x, y, c=z, vmin=0, vmax=40, cmap='viridis')
 plt.colorbar(label='elevation (m)')
 plt.title('Mountain Elevation')
 plt.xlabel('x coordinate (m)')
