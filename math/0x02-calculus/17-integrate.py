660d98a88a51f3581e79a13a14b6ca0d22ea2da5
ralexrivero <ralexrivero@gmail.com>
2022-05-19 23:35:14 +0000

    polynomial integral

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,4 +4,15 @@
 
 def poly_integral(poly, C=0):
     """ return of a polynomial """
-    return poly
+    if not isinstance(poly, list) or poly is None:
+        return None
+    if C is None or not isinstance(C, int):
+        return None
+    polyInt = []
+    polyInt.append(C)
+    for i, coef in enumerate(poly):
+        integral = (coef / (i + 1))
+        if float(integral).is_integer():
+            integral = int(integral)
+        polyInt.append(integral)
+    return polyInt
