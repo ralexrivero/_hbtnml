07ce4a06fe6e0b519ea9b38d17ce8523aeb473e6
ralexrivero <ralexrivero@gmail.com>
2022-05-19 21:30:11 +0000

    len 0 and 1

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,10 +6,12 @@ def poly_derivative(poly):
     """ calculates the derivative of a polynomial """
     if not isinstance(poly, list) or poly is None:
         return None
-    if len(poly) < 2:
+    if len(poly) == 1:
+        return [0]
+    if len(poly) == 0:
         return None
-    deri = []
 
+    deri = []
     for i, coef in enumerate(poly):
         if i > 0:
             deri.append(coef * i)
