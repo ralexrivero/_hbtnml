8cc3ad9785cacb7555189a6994a3d970478d57fd
ralexrivero <ralexrivero@gmail.com>
2022-05-19 15:49:34 +0000

    delete the word 'for' in a comment

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,7 +6,7 @@ sum = 0
 def summation_i_squared(n):
     """ calculates summation of squared """
     if not isinstance(n, (int, float)):
-        return None  # check for a valid number
+        return None  # check the valid number
 
     sum = n * (n + 1) * (2 * n + 1) / 6
 
