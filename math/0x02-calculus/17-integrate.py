1793bd231df14767da80ba080884b605f9fd4229
ralexrivero <ralexrivero@gmail.com>
2022-05-19 23:45:43 +0000

    add length 0 and 1

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,12 +4,16 @@
 
 def poly_integral(poly, C=0):
     """ return of a polynomial """
-    if not isinstance(poly, list) or poly is None:
+    if not isinstance(poly, list) or poly is None or len(poly) == 0:
         return None
     if C is None or not isinstance(C, int):
         return None
     polyInt = []
     polyInt.append(C)
+
+    if len(poly) == 1 and poly[0] == 0:
+        return polyInt
+
     for i, coef in enumerate(poly):
         integral = (coef / (i + 1))
         if float(integral).is_integer():
--- math/0x02-calculus/17-main.py
+++ math/0x02-calculus/17-main.py
@@ -4,3 +4,24 @@ poly_integral = __import__('17-integrate').poly_integral
 
 poly = [5, 3, 0, 1]
 print(poly_integral(poly))
+
+# main_0 normal
+print(poly_integral([7, 4, 6, 1, 5]))
+print(poly_integral([4, 8, 2, 4, 7, 1, 9], C=5))
+
+# main_1 skipped powers
+print(poly_integral([6, 5, 0, 0, -3]))
+print(poly_integral([0, 0, 3, -1, 0, 6, 2], C=6))
+
+# main_4 only a constant
+print(poly_integral([5]))
+print(poly_integral([5], C=7))
+
+# main_2 poly is [0]
+print(poly_integral([0]))
+print(poly_integral([0], C=7))
+
+# main_3 invalid poly
+print(poly_integral(5))
+print(poly_integral([]))
+print(poly_integral([5], C=None))
