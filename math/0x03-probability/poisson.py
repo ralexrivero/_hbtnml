59be1ee4431c2aca54de2ae8ea59ec9771c7243b
ralexrivero <ralexrivero@gmail.com>
2022-07-11 00:15:15 -0300

    poisson file

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """ Represents a poisson distribution """
 
+class Poisson:
+    """ represents a poisson distribution """
+    def __init__(self, data=None, lambtha=1.):
+        """ class constructor """
+        self.data = data
+        self.lambtha = lambtha
 
-def __init__(self, data=None, lambtha=1.):
-    pass
+        pass
