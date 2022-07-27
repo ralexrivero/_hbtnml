23bb79dfac4b064b81696f2c78a3e82a1e1821a4
ralexrivero <ralexrivero@gmail.com>
2022-07-27 21:05:38 +0000

    simplified version

--- math/0x00-linear_algebra/7-gettin_cozy_2.py
+++ math/0x00-linear_algebra/7-gettin_cozy_2.py
@@ -4,21 +4,28 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """ concatenates two matrices """
+    if type(mat1) is not list\
+       or type(mat2) is not list\
+       or not len(mat1)\
+       or not len(mat2):
+        return None
     if (axis != 0 and axis != 1):
         return None
-    matCon = []
-    if (axis == 0):
+    elif axis == 0:
         if (len(mat1[0]) != len(mat2[0])):
             return None
-        for row1 in mat1:
-            matCon.append(row1.copy())
-        for row2 in mat2:
-            matCon.append(row2.copy())
-    elif (axis == 1):
+        matCon = []
+        for row in mat1:
+            matCon.append(row.copy())
+        for row in mat2:
+            matCon.append(row.copy())
+    else:
         if (len(mat1) != len(mat2)):
             return None
+        matCon = []
         for i in range(len(mat1)):
             matCon.append([])
             matCon[i] = mat1[i] + mat2[i]
-
+    if type(matCon[0]) is not list or type(matCon) is not list:
+        return None
     return matCon
