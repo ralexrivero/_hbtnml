a5e89753ade28ae421c6051af3657d1ad1a289a9
ralexrivero <ralexrivero@gmail.com>
2022-07-27 17:43:52 +0000

    transpose matrix without comprehension list

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -4,5 +4,10 @@
 
 def matrix_transpose(matrix):
     """ transpose matrix """
-    transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    transposed = []
+    for x in range(len(matrix[0])):
+        transposed_row = []
+        for row in matrix:
+            transposed_row.append(row[x])
+        transposed.append(transposed_row)
     return transposed
