1c0647e5363830139a4dfa63bfa2d4940aca0856
ralexrivero <ralexrivero@gmail.com>
2022-07-27 18:50:46 +0000

    clean code

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """ add two matrices """
-sum = []
-sumSub = []
 
 
 def add_matrices2D(mat1, mat2):
     """ add two matrices """
+
     if len(mat1[0]) != len(mat2[0]):
         return None
-    for i in range(len(mat1)):
-        for j in range(len(mat1[i])):
-            sumSub.append(mat1[i][j] + mat2[i][j])
-        sum.append(sumSub[:])
-        sumSub.clear()
+
+    sum = []
+    for x in range(len(mat1)):
+        sumSub = []
+        for y in range(len(mat1[x])):
+            sumSub.append(mat1[x][y] + mat2[x][y])
+        sum.append(sumSub)
     return sum
