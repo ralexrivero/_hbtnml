f5ef3bcd26cb32941099de6cdcd86138d956d0a0
ralexrivero <ralexrivero@gmail.com>
2022-07-29 01:08:22 +0000

    simplify code

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -5,7 +5,6 @@ import numpy as np
 
 def np_slice(matrix, axes={}):
     """ slice a matrix """
-    sliced = []
     obj = [slice(None)] * (max(axes) + 1)
     for key, val in axes.items():
         obj[key] = slice(*val)
