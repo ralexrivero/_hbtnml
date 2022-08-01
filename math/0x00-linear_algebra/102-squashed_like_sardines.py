30104e8f3d1f43affb2a802c2a9bf383f0f13849
ralexrivero <ralexrivero@gmail.com>
2022-08-01 01:16:45 +0000

    concatenate matrices

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -17,7 +17,69 @@ def cat_matrices(mat1, mat2, axis=0):
     cated = []
     s1 = matrix_shape(mat1)
     s2 = matrix_shape(mat2)
-    if s1 != s2:
+
+    ndim = len(s1)
+
+    if s1 != s2:  # control shape
         return None
+    elif ndim <= axis:  # control axis on dimension
+        return None
+    elif ndim == 1:  # 1D Array
+        if axis == 0:
+            cated = mat1 + mat2
+        else:
+            return None
+    elif ndim == 2:  # 2D Array
+        if axis == 0:
+            for row1 in mat1:
+                cated.append(row1)
+            for row2 in mat2:
+                cated.append(row2)
+        elif axis == 1:
+            for i in range(s1[0]):
+                cated.append([])
+                cated[i].append(mat1[i] + mat2[i])
+        else:
+            return None
+    elif ndim == 3:  # 3D Array
+        if axis == 0:
+            for row1 in mat1:
+                cated.append(row1)
+            for row2 in mat2:
+                cated.append(row2)
+        elif axis == 1:
+            for i in range(s1[0]):
+                cated.append([])
+                cated[i].append(mat1[i] + mat2[i])
+        elif axis == 2:
+            for i in range(s1[0]):
+                for j in range(s1[1]):
+                    cated.append([mat1[i][j] + mat2[i][j]])
+        else:
+            return
+    elif ndim == 4:  # 4D Array
+        if axis == 0:
+            for row1 in mat1:
+                cated.append(row1)
+            for row2 in mat2:
+                cated.append(row2)
+        elif axis == 1:
+            for i in range(s1[0]):
+                cated.append([])
+                cated[i].append(mat1[i] + mat2[i])
+        elif axis == 2:
+            for i in range(s1[0]):
+                for j in range(s1[1]):
+                    cated.append([mat1[i][j] + mat2[i][j]])
+        elif axis == 3:
+            for i in range(s1[0]):
+                for j in range(s1[1]):
+                    cated.append([])
+                    for k in range(s1[2]):
+                        cated[i].append([mat1[i][j][k] + mat2[i][j][k]])
+        else:
+            return
     else:
-        return cated
+        return cated  # Not handled more than 4 dimensional arrays
+
+    return cated
