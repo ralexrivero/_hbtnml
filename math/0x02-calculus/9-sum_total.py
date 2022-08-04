484cc5e00922d8b18d65e495c14b65df07770c9a
ralexrivero <ralexrivero@gmail.com>
2022-08-04 01:12:45 +0000

    border case

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """ calculates summation of squared """
-    if not isinstance(n, (int, float)) or n < 0:
+    if not isinstance(n, (int, float)) or n <= 0:
         return None  # check the valid number
 
     if n == 1:
