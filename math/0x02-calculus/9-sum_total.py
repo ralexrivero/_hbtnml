7af2a5591d5a3380fb6913a9a6d53cb274198d22
ralexrivero <ralexrivero@gmail.com>
2022-08-04 02:10:36 +0000

    not recursion, Faulhaber

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -3,11 +3,15 @@
 
 
 def summation_i_squared(n):
-    """ calculates summation of squared recursively """
+    """
+    calculates summation of squared
+    using Faulhaber's formula
+    """
     if not isinstance(n, (int, float)) or n <= 0:
         return None  # check the valid number
 
     if n == 1:
         return 1
     else:
-        return summation_i_squared(n - 1) + (n * n)
+        # return summation_i_squared(n - 1) + (n * n)
+        return int(n * (n + 1) * (2 * n + 1) / 6)
